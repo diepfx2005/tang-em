0a87178811b380b78bdf9ca8e3a3a40f675f6e17
diepfx2005 <85275897+diepfx2005@users.noreply.github.com>
2021-06-03 14:18:19 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+introTitle: 'Em à!',
+introDesc: `Trái đất vốn lạ thường
+Mà sao em cứ đi nhầm đường
+Lạc vào tim anh lẻ loi
+Đằng sau chữ yêu đây là thương`,
+btnIntro: 'hihi',
+title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+desc: 'Em có yêu anh không ',
+btnYes: 'Có, Em yêu anh <33',
+btnNo: 'Không, Em xl :3',
+question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+btnReply: 'Gửi cho anh <3',
+reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+messDesc: 'Từ nay,em không cần phải lo chuyện thế giới nữa đã có anh rồi.',
+btnAccept: 'Dạ, anh <3',
+messLink: 'http://m.me/diepfx2005
+}
